47e71b76d7d6f3956e782b564e3bae8ceab87392
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-21 13:35:30 +0100

    Creato il componente dell'header SearchBar;

---
+++ src/components/components-header/SearchBar.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: "SearchBar",
+
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" placeholder="Search...">
+        <div class="icon_search">
+            <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+        </div>
+    </div>
+</template> 
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables.scss' as *;
+@use '../../styles/partials/mixins.scss' as *;
+
+div {
+    width: fit-content;
+    height: 100%;
+    @include center();
+
+    input {
+        background-color: $secondColor;
+        font-size: 16px;
+        height: 45px;
+        line-height: 45px;
+        padding-left: 20px;
+        border: none;
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+
+    .icon_search {
+        background-color: $secondColor;
+        color: $thirdColor;
+        font-size: 15px;
+        height: 45px;
+        width: 30px;
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -12,10 +12,11 @@ import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 /* add icons to the library */
-library.add(faUserSecret, faAngleDown, faCartShopping, faCircleUser);
+library.add(faUserSecret, faAngleDown, faCartShopping, faCircleUser, faMagnifyingGlass);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
