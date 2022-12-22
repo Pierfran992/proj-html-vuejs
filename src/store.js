55450c5ee02aa23ddca38573638dd42cca5d6bd4
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-22 15:59:11 +0100

    Creazione dei dati da richiamare nel footer;

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
+    // dati per la navBar dell'header
     navBarLink: [
         {
             text: "Home",
@@ -27,4 +28,28 @@ export const store = reactive({
             url: "#",
         },
     ],
+
+    // dati per il footer
+    address: [
+        "382 NE 191st St # 87394 Miami, FL 33179-3899",
+        "+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)",
+        "support@maxcoach.com",
+    ],
+
+    explore: [
+        "Start here",
+        "Blog",
+        "About us",
+        "Success story",
+        "Courses",
+        "Contact us",
+    ],
+
+    information: [
+        "Membership",
+        "Purchase guide",
+        "Privacy policy",
+        "Terms of services",
+    ]
+
 });
