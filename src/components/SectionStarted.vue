20fd968606e71445be7c3b3d731a9d64e8621089
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-22 14:48:50 +0100

    Completato lo sviluppo della SectionStarted;

--- src/components/SectionStarted.vue
+++ src/components/SectionStarted.vue
@@ -6,7 +6,16 @@ export default {
 
 <template>
     <section>
+        <div class="container">
+            <!-- titoli della section -->
+            <h4>Start today for getting <span>Online certification</span></h4>
+            <h3>You can be your own guiding star with our help!</h3>
 
+            <!-- bottone per l'iscrizione -->
+            <div class="button">
+                Get started now
+            </div>
+        </div>
     </section>
 </template>
 
@@ -16,5 +25,33 @@ export default {
 
 section {
     padding: 100px 0;
+}
+
+.container {
+    text-align: center;
+    color: $fontPrimaryColor;
+
+    h4 {
+        font-size: 20px;
+
+        span {
+            color: $fontSecondaryColor;
+            font-weight: 400;
+        }
+    }
+
+    h3 {
+        font-size: 30px;
+        margin-top: 10px;
+    }
+
+    .button {
+        @include button();
+        margin: 30px auto 0 auto;
+
+        &:hover {
+            background-color: $firstColor;
+        }
+    }
 }
 </style>
